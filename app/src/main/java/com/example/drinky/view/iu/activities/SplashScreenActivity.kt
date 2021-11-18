522912722a0f795fcb977ea34385902871899138
Martha <ing.alvarez.edu@gmail.com>
2021-11-17 23:47:24 -0500

    Splash added

---
+++ app/src/main/java/com/example/drinky/view/iu/activities/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.example.drinky.view.iu.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import com.example.drinky.R
+
+@Suppress("DEPRECATION")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
+
+    }
+}
